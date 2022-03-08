7342dc091076e9f678dcef703ad7cfad1ca8f4e2
Ghazi <ghazi@bilog.fr>
2022-03-08 11:55:56 +0100

    set Min et Max date de naissance

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -15,16 +15,22 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
     this.reactiveForm = new FormGroup({
       firstname: new FormControl('', [Validators.required]),
-      lastname: new FormControl(''),
-      email: new FormControl(''),
-      address: new FormGroup({
-        address: new FormControl(''),
-        city: new FormControl(''),
-        state: new FormControl(''),
-      })
+      lastname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required]),
+      tel: new FormControl('', [Validators.required]),
+      birthDate: new FormControl('', [Validators.required]),
     })
+
+    this.maxDate();
   }
 
+  maxDate() {
+    var today: any;
+    today = new Date();
+    today = today.getFullYear()-18+'-'+("0"+(today.getMonth()+1)).slice(-2)+'-'+("0"+today.getDate()).slice(-2);
+    (document.getElementById("birthDate") as HTMLInputElement)?.setAttribute("max", today);
+  }
 
   onSubmit(templateForm: any) {
 
