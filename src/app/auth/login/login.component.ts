4b7c085e1a234ab57b81f5651076ef40530b31cf
YassineBenJlila <benjlila.yassine@gmail.com>
2022-03-09 14:07:35 +0100

    feature: firt commit of login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  reactiveForm: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.reactiveForm = new FormGroup({
+      firstname: new FormControl('', [Validators.required]),
+      lastname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required]),
+      tel: new FormControl('', [Validators.required]),
+      birthDate: new FormControl('', [Validators.required]),
+    })
   }
 
+  onLogin(templateForm: any) {
+  }
 }
