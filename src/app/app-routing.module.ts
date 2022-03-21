67f44a60a1156076a71e38eb432c3b35bdee8278
gazifie <gazifie@yahoo.fr>
2022-03-21 15:50:45 +0100

    Interface user + reservation + ajout placeholders pour les form register et login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { BoardUserComponent } from './content/board-user/board-user.component';
 import { PublicPageComponent } from './content/public-page/public-page.component';
 
 const routes: Routes = [
   {path:'welcome', component: PublicPageComponent},
   {path:'login', component: LoginComponent},
   {path:'register', component: RegisterComponent},
+  {path:'user-board', component: BoardUserComponent},
 
   {path: '', redirectTo: 'welcome', pathMatch: 'full'},
 
--- src/app/content/board-user/board-user.component.ts
+++ src/app/content/board-user/board-user.component.ts
@@ -7,9 +7,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardUserComponent implements OnInit {
 
+  cars: any;
+  regions: any;
+  car_id : any;
+  baseUrl = "/assets/images/cars/";
   constructor() { }
 
   ngOnInit(): void {
+    var id = 0;
+    this.cars = [
+      {
+        id: id++, model: 'Mazda', photo: this.baseUrl + 'mazda.png', price: '150dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: id++, model: 'Mercedes', photo: this.baseUrl + 'mercedes.png', price: '320dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: id++, model: 'F-Type', photo: this.baseUrl + 'Jaguar-F-TYPE.png', price: '350dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: id++, model: 'Audi RS7', photo: this.baseUrl + 'audi_rs7.png', price: '350dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: id++, model: 'Range Rover', photo: this.baseUrl + 'rangerover.png', price: '300dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: id++, model: 'Zuzuki', photo: this.baseUrl + 'zuzuki.png', price: '150dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: id++, model: 'Toyota Camry', photo: this.baseUrl + 'toyota_camry.png', price: '150dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: id++, model: 'VW Golf 8', photo: this.baseUrl + 'vw_golf8.png', price: '250dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: id++, model: 'Porsche', photo: this.baseUrl + 'porsche.png', price: '500dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: id++, model: 'Mercedes G Class', photo: this.baseUrl + 'mercedes_Gclass.png', price: '350dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: id++, model: 'BMW M3', photo: this.baseUrl + 'BMW_m3.png', price: '350dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: id++, model: 'BMW i8', photo: this.baseUrl + 'BMW_i8.png', price: '450dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: id++, model: 'Nissan GTR', photo: this.baseUrl + 'nissan_gtr.png', price: '400dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: id++, model: 'Ford Mustang', photo: this.baseUrl + 'ford_mustang.png', price: '400dt', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      },
+    ]
+
+    id = 0;
+    this.regions = [
+      {id: id++, label: 'Tunis' },
+      {id: id++, label: 'Ariana' },
+      {id: id++, label: 'Nabeul' },
+      {id: id++, label: 'Sousse' },
+      {id: id++, label: 'Sfax' },
+      {id: id++, label: 'Djerba' },
+    ]
+
+    console.table(this.cars);
+    console.table(this.regions);
+
+    // Get the <span> element that closes the modal
+    var modal = document.getElementById("myModal") as HTMLDivElement;
+    var span = document.getElementsByClassName("close")[0] as HTMLSpanElement;
+    span.onclick = function () {
+      modal.style.opacity = "0";
+      modal.style.pointerEvents = "none";
+    }
+
   }
 
+  showModal(id: any): void {
+    // document.querySelector("#modal-dialog")?.showModal();
+    console.log(id);
+    this.car_id = id;
+
+    // Get the modal
+    var modal = document.getElementById("myModal") as HTMLDivElement;
+
+    // When the user clicks the button, open the modal 
+    modal.style.opacity = "1";
+    modal.style.pointerEvents = "auto";
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.opacity = "0";
+        modal.style.pointerEvents = "none";
+      }
+    }
+  }
 }
